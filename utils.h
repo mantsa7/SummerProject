4b9e2a76fa577ade18f09800ddbeab43984f79b1
ovi-k <65507083+ovi-k@users.noreply.github.com>
2020-06-09 23:11:05 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,56 +1,52 @@
-#include "utils.h"
+#ifndef CPP_FTP_UTILS_H
+#define CPP_FTP_UTILS_H
 
-loggerT::loggerT(std::string_view logFileName) {
-	if (logFileName == "")
-		logFile = std::make_unique<std::ofstream>(nullptr);
-	else {
-		logFile = std::make_unique<std::ofstream>(std::ofstream(logFileName.data()));
-		std::cout << "Logging to file " << logFileName << std::endl;
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
+// for working with filesystem
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+
+// endline for logger
+enum loggerEndl {ENDL};
+
+// logger class which outputs to stdout as well to log
+// if we don't supply a log file name then we simply set the logfile ptr to nullptr
+struct loggerT {
+
+	// unique_ptr for easier checking
+	// also it will automatically close on really bad errors
+	std::unique_ptr<std::ofstream> logFile;
+
+	loggerT(std::string_view);
+
+	// operators for outputting various values
+	template<typename T>
+	loggerT& operator<<(T value) {
+		std::cout << value;
+		if (logFile)
+			*logFile << value;
+		return *this;
 	}
-}
 
-loggerT& loggerT::operator<<(loggerEndl) {
-	std::cout << std::endl;
-	if (logFile)
-		*logFile << std::endl;
-	return *this;
-}
+	// only accept the specific ENDL value
+	// send std::endl to both streams, effectively flushing them
+	loggerT& operator<<(loggerEndl);
 
-void loggerT::close() {
-	if (logFile)
-		logFile->close();
-}
+	// correctly handle the closing of required file
+	void close();
+};
 
-std::pair<std::string, std::string> getNextParam(std::string_view str) {
-	auto pos = str.find(' ');
-	if (pos == std::string::npos)
-		return std::pair<std::string, std::string>{str, ""};
-	return std::pair<std::string, std::string>{str.substr(0, pos), str.substr(pos + 1)};
-}
+// function which returns current parameter and the rest of the string (separated by space)
+std::pair<std::string, std::string> getNextParam(std::string_view);
 
-std::vector<std::string_view> splitByDelim(std::string_view str, std::string_view delim) {
-	if (str == "")
-		return {};
-	int32_t pos = str.find(delim);
-	if (pos == std::string::npos)
-		return {str};
-	std::vector<std::string_view> tmp {str.substr(0, pos)};
-	std::vector<std::string_view> append = splitByDelim(str.substr(pos + 1), delim);
-	tmp.insert(tmp.end(), append.begin(), append.end());
-	return tmp;
-}
+// recursively splits a string into vector by delimiter
+std::vector<std::string_view> splitByDelim(std::string_view, std::string_view);
 
-std::string getFilePerms (fs::path path) {
-	const fs::file_status fileStat = fs::status(path);
-	const fs::perms filePerms = fileStat.permissions();
-	return std::string{} + (fs::is_directory(fileStat) ? "d" : "-") +
-		   ((filePerms & fs::perms::owner_read) != fs::perms::none ? "r": "-") +
-		   ((filePerms & fs::perms::owner_write) != fs::perms::none ? "w": "-") +
-		   ((filePerms & fs::perms::owner_exec) != fs::perms::none ? "x": "-") +
-		   ((filePerms & fs::perms::group_read) != fs::perms::none ? "r": "-") +
-		   ((filePerms & fs::perms::group_write) != fs::perms::none ? "w": "-") +
-		   ((filePerms & fs::perms::group_exec) != fs::perms::none ? "x": "-") +
-		   ((filePerms & fs::perms::others_read) != fs::perms::none ? "r": "-") +
-		   ((filePerms & fs::perms::others_write) != fs::perms::none ? "w": "-") +
-		   ((filePerms & fs::perms::others_exec) != fs::perms::none ? "x": "-");
-}
+// returns a string of file permissions
+// linux-like way
+std::string getFilePerms (fs::path);
+
+#endif //CPP_FTP_UTILS_H
